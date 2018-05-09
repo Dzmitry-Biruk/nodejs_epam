5458199f6c1f686346da476b0ff282b91e10109d
DzmitryBiruk <birukdmitry96@gmail.com>
2018-05-09 21:59:48 +0300

    Add stracture and test data

---
+++ app.js
@@ -0,0 +1,5 @@
+import DirWatcher from './modules/dirwatcher';
+import Importer from './modules/importer';
+
+const dir = new DirWatcher();
+const imp = new Importer();
---
+++ modules/dirwatcher.js
@@ -0,0 +1,5 @@
+export default class DirWatcher {
+  constructor() {
+    console.log('DirWatcher');
+  }
+}
