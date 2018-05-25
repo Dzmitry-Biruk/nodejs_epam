d8da1cccf606e87ba23815ecde7245302ccd078b
DzmitryBiruk <birukdmitry96@gmail.com>
2018-05-25 19:52:21 +0300

    Add basic shell script

---
+++ utils/actions.js
@@ -0,0 +1,5 @@
+function reverse(str) { /* ... */ }
+function transform(str) { /* ... */ }
+function outputFile(filePath) { /* ... */ }
+function convertFromFile(filePath) { /* ... */ }
+function convertToFile(filePath) { /* ... */ }
